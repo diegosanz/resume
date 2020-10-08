da4606f5f4573079c8cb40185e0b00ceb076d89c
Diego <diego2sanz@gmail.com>
2020-10-08 21:30:20 +0200

    First commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Page } from "./../Page/Page";
+
+function App() {
+  return (
+    <>
+      <Page>Página 1</Page>
+      <Page>Página 2</Page>
+    </>
+  );
+}
+
+export default App;
