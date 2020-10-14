ee4f264a632b638de628e74561a73447334b2858
Diego <diego2sanz@gmail.com>
2020-10-14 22:32:15 +0200

    ADD main structure

--- src/components/ui/Sheet.js
+++ src/components/ui/Sheet.js
@@ -1,5 +1,5 @@
 import React from "react";
 
 export const Sheet = ({ children }) => {
-  return <section className="sheet padding-10mm">{children}</section>;
+  return <div className="sheet padding-10mm">{children}</div>;
 };
