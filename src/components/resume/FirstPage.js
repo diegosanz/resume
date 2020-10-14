266fdd9fbed825a141a14747b907704b5ceb80d2
Diego <diego2sanz@gmail.com>
2020-10-14 21:07:46 +0200

    ADD contacts to FirstPage

--- src/components/resume/FirstPage.js
+++ src/components/resume/FirstPage.js
@@ -1,7 +1,12 @@
 import React from "react";
+import { ContactSection } from "../ui/ContactSection";
 import { MainSubtitle } from "../ui/MainSubtitle";
 import { MainTitle } from "../ui/MainTitle";
 import { Sheet } from "../ui/Sheet";
+import { ContactGithub } from "./ContactGithub";
+import { ContactLocation } from "./ContactLocation";
+import { ContactMail } from "./ContactMail";
+import { ContactPhone } from "./ContactPhone";
 
 export const FirstPage = () => {
   return (
@@ -11,6 +16,12 @@ export const FirstPage = () => {
           Diego Sanz
           <MainSubtitle>Fullstack developer</MainSubtitle>
         </MainTitle>
+        <ContactSection>
+          <ContactMail></ContactMail>
+          <ContactPhone></ContactPhone>
+          <ContactLocation></ContactLocation>
+          <ContactGithub></ContactGithub>
+        </ContactSection>
       </header>
     </Sheet>
   );
