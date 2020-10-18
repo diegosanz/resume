287e44c611da717a07fc27d4f2b3fb923745d34d
Diego <diego2sanz@gmail.com>
2020-10-18 21:58:04 +0200

    ADD Skill template

---
+++ _templates/skills/new/prompt.js
@@ -0,0 +1,22 @@
+module.exports = [
+  {
+    type: "input",
+    name: "name",
+    message: "Skill name",
+  },
+  {
+    type: "input",
+    name: "percentage",
+    message: "Percentage",
+  },
+  {
+    type: "input",
+    name: "mainColor",
+    message: "Main color (in case of HEX code use #)",
+  },
+  {
+    type: "input",
+    name: "textColor",
+    message: "Text color (in case of HEX code use #)",
+  },
+];
