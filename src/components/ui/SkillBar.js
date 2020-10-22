de131ed518e95b8e2b64b3990df5461029a3ccfc
Diego <diego2sanz@gmail.com>
2020-10-22 21:52:28 +0200

    BUGFIX and some improvements to SkillBar.js

--- src/components/ui/SkillBar.js
+++ src/components/ui/SkillBar.js
@@ -1,14 +1,14 @@
 import styled from "styled-components";
 
-const padding = "0.2rem";
+const padding = "0.25rem 0 0.25rem 1rem";
 
 export const SkillBar = styled.div`
   width: 100%;
   padding: ${padding};
   border: 2px solid ${(props) => props.mainColor || "black"};
   position: relative;
-  overflow: hidden;
   font-weight: bold;
+  margin: 0.2rem;
 
   &::before {
     content: "${(props) => props.children}";
@@ -18,16 +18,35 @@ export const SkillBar = styled.div`
     height: 100%;
     top: 0;
     left: 0;
-    background-color: ${(props) => props.mainColor || "black"};
-    width: ${(props) => props.percentage || 0}%;
+    z-index: 1;
     overflow: hidden;
+    width: ${(props) => props.percentage || 0}%;
+    /* FIXME: no bold font */
     font-weight: bold;
 
-    /** HACK: (When printing) For browser config "Print Background colours and images" turned off */
     @media print {
-      box-shadow: inset 0 0 0 1000px ${(props) => props.mainColor || "black"};
+      /* HACK: when printing, some browsers turn the white fonts grey */
       color: transparent;
-      text-shadow: 2px 0 ${(props) => props.textColor || "white"};
+      text-shadow: 0 0 ${(props) => props.textColor || "white"};
+    }
+  }
+
+  &::after {
+    content: "";
+    position: absolute;
+    height: 100%;
+    background-color: ${(props) => props.mainColor || "black"};
+    overflow: hidden;
+
+    /* HACK: there are some glitches when it is printed as a pdf between the background/box-shadow and the parent border, so we expand it one pixel at the top, left and bottom side */
+    top: -1px;
+    left: -1px;
+    height: calc(100% + 2px); // 2px => 1px top + 1px bottom;
+    width: calc(${(props) => props.percentage || 0}% + 1px);
+
+    @media print {
+      /* HACK: When printing, for browsers with config "Print Background colours and images" turned off */
+      box-shadow: inset 0 0 0 1000px ${(props) => props.mainColor || "black"};
     }
   }
 `;
