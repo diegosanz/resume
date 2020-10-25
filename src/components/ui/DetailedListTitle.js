16a7e2be687405f07baf99b568f836676b1a18f0
Diego <diego2sanz@gmail.com>
2020-10-25 13:43:03 +0100

    ADD square black titles

--- src/components/ui/DetailedListTitle.js
+++ src/components/ui/DetailedListTitle.js
@@ -2,7 +2,16 @@ import styled from "styled-components";
 
 export const DetailedListTitle = styled.h2`
   font-family: ${(props) => props.theme.fontAlt};
-  border-bottom: 2px solid black;
-  padding-bottom: 0.2rem;
-  margin: 0.5rem 0 0.75rem;
+  color: white;
+  background-color: black;
+  display: inline-block;
+  padding: 0.25rem 0.5rem;
+  margin: 0.75rem 0 0.5rem;
+
+  @media print {
+    /* HACK: when printing, some browsers turn the white fonts grey */
+    color: transparent;
+    text-shadow: 0 0 white;
+    box-shadow: inset 0 0 0 1000px black;
+  }
 `;
